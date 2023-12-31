3040c8b4a3092ba19e3797bb94f20054acf7d703
Victor Ochoa <73369179+vmochoa@users.noreply.github.com>
2023-12-31 02:16:39 -0600

    ataque en todas direcciones

--- Jugador.gd
+++ Jugador.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 var velocidad = 150
 # Última dirección en la que el personaje miró
 var ultima_direccion = Vector2.DOWN
+# Timer para controlar los ataques
+var timer_de_ataque: Timer
+# Control de ataque
+var puede_atacar = true
 
 # Referencia al AnimationTree y AnimationNodeStateMachinePlayback
 var animation_tree: AnimationTree
@@ -13,18 +17,41 @@ func _ready():
 	animation_tree = $AnimationTree
 	state_machine = animation_tree.get("parameters/playback") as AnimationNodeStateMachinePlayback
 	animation_tree.active = true
+	timer_de_ataque = $AttackTimer
+var ultima_tecla_presionada = Vector2.ZERO
 
 func _process(delta):
-	var direccion = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direccion * velocidad
+	var input_x = int(Input.is_action_pressed("moveRight")) - int(Input.is_action_pressed("moveLeft"))
+	var input_y = int(Input.is_action_pressed("moveDown")) - int(Input.is_action_pressed("moveUp"))
 
-   # Actualizar la última dirección si está moviéndose
+	# Actualizar última tecla presionada
+	if input_x != 0:
+		ultima_tecla_presionada.x = input_x
+	if input_y != 0:
+		ultima_tecla_presionada.y = input_y
+
+	var direccion = Vector2(input_x, input_y)
+	if direccion.length() > 1:
+		direccion = direccion.normalized()
+
+	# Aplicar movimiento y actualizar animación
 	if direccion != Vector2.ZERO:
+		velocity = direccion * velocidad
 		ultima_direccion = direccion
+		move_and_slide()
+		actualizar_animacion_movimiento(direccion)
+	elif not Input.is_action_pressed("ui_attack"):
+		state_machine.travel("idle")
 
-	move_and_slide()
+	# Manejo de la acción de ataque
+	if Input.is_action_pressed("ui_attack") and puede_atacar:
+		
+		inicia_ataque()
 
-	# Determinar y ejecutar la animación de movimiento
+func actualizar_animacion_movimiento(direccion: Vector2):
+	if not timer_de_ataque.is_stopped():
+		return
+
 	if direccion.y < 0:
 		state_machine.travel("caminarEspalda")
 	elif direccion.y > 0:
@@ -33,17 +60,19 @@ func _process(delta):
 		state_machine.travel("caminarDerecha")
 	elif direccion.x < 0:
 		state_machine.travel("caminarIzquierda")
-	else:
-		if not Input.is_action_pressed("ui_attack"):
-			state_machine.travel("idle")
 
-	 # Manejo de la acción de ataque
-	if Input.is_action_pressed("ui_attack"):
-		if ultima_direccion.y < 0:
-			state_machine.travel("atacarEspalda")
-		elif ultima_direccion.y > 0:
-			state_machine.travel("atacarFrente")
-		elif ultima_direccion.x > 0:
-			state_machine.travel("atacarDerecha")
-		elif ultima_direccion.x < 0:
-			state_machine.travel("atacarIzquierda")
+func inicia_ataque():
+	if ultima_direccion.y < 0:
+		state_machine.travel("atacarEspalda")
+	elif ultima_direccion.y > 0:
+		state_machine.travel("atacarFrente")
+	elif ultima_direccion.x > 0:
+		state_machine.travel("atacarDerecha")
+	elif ultima_direccion.x < 0:
+		state_machine.travel("atacarIzquierda")
+
+	puede_atacar = false
+	timer_de_ataque.start()
+
+func _on_attack_timer_timeout():
+	puede_atacar = true
